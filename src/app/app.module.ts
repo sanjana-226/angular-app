77952648577b4e55f4245349d254511473b6043f
sanjana-226 <69665875+sanjana-226@users.noreply.github.com>
2023-07-24 17:44:49 +0530

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
+import { TodoComponent } from './todo/todo.component';
+import { PostsComponent } from './posts/posts.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, TodoComponent, PostsComponent, AlbumComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
